569a331ee056f8d631a57bb96562f705239886f6
Laguna1 <lagunazoomarket@gmail.com>
2020-12-02 19:16:59 +0200

    Create Project class

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import Todo from './todo'
+import Project from './project'
+
 const container = document.getElementById("content");
 const todo1 = new Todo('Plan of project', 'Brainstorm and design', '02/12/20', 'high');
-container.innerHTML = todo1.description;
+const defaultProject = new Project('Main project');
+defaultProject.todos.push(todo1);
+
+container.innerHTML = defaultProject.name;
+
+
---
+++ src/project.js
@@ -0,0 +1,7 @@
+class Project {
+  constructor(name) {
+    this.name = name;
+    this.todos = [];
+  }
+}
+export default Project;
