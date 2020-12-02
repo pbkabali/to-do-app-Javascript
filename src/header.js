73b68aabfbcfae6784bf1a28199054d6967f2700
Laguna1 <lagunazoomarket@gmail.com>
2020-12-02 22:33:28 +0200

    Add button to header

--- src/header.js
+++ src/header.js
@@ -1,14 +1,33 @@
 const header = (heading) => {
   const element = document.createElement("div");
   element.classList.add(
+    "d-flex",
+    "flex-row-reverse",
+    "justify-content-around",
+    "border-bottom"
+  );
+  const element1 = document.createElement("div");
+
+  element1.classList.add(
     "text-center",
     "my-3",
     "font-weight-bold",
     "text-uppercase",
-    "border-bottom",
     "pb-3"
   );
-  element.innerText = heading;
+  element1.innerText = heading;
+
+  const element2 = document.createElement("div");
+  element2.classList.add(
+    "text-center",
+    "my-3",
+    "btn",
+    "btn-large",
+    "btn-primary"
+  );
+  element2.innerText = "See all projects";
+  element.appendChild(element1);
+  element.appendChild(element2);
   return element;
 };
 
