80891b3fd3b3e9cee472aeaff3403b4cbe71a205
Paul Balitema Kabali <engineerbpk@gmail.com>
2020-12-18 01:37:41 +0300

    Add test for confirmation modal

--- src/confirm.test.js
+++ src/confirm.test.js
@@ -1,13 +1,19 @@
-import {hideConfirmView} from "./confirm";
-
+import confirm, { hideConfirmView } from "./confirm";
 
 test("clears the confirm window", () => {
-    const confirmWindow = document.createElement("div");
-    confirmWindow.id = 'notification-div';
-    document.body.appendChild(confirmWindow);
-    confirmWindow.innerText = "CONFIRM TEXT";
-    hideConfirmView();
-    expect(confirmWindow.outerHTML).toBe("<div id=\"notification-div\"></div>");
-    expect(confirmWindow.id).toBe('notification-div');
-    expect(confirmWindow.innerText).toBe("");
+  const confirmWindow = document.createElement("div");
+  confirmWindow.id = "notification-div";
+  document.body.appendChild(confirmWindow);
+  confirmWindow.innerText = "CONFIRM TEXT";
+  hideConfirmView();
+  expect(confirmWindow.outerHTML).toBe('<div id="notification-div"></div>');
+  expect(confirmWindow.id).toBe("notification-div");
+  expect(confirmWindow.innerText).toBe("");
+});
+
+test("creates the confirm modal", () => {
+  const action = jest.fn();
+  const hideConfirmView = jest.fn();
+  const msg = "delete?";
+  expect(confirm(msg, action).id).toBe("confirm");
 });
