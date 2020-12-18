ccab23c8382d2c12d4da82399bc00eae3246fb4e
Laguna1 <lagunazoomarket@gmail.com>
2020-12-18 18:11:00 +0200

    Add tests for the Todo class

--- src/confirm.test.js
+++ src/confirm.test.js
@@ -1,18 +1,23 @@
 import confirm, { hideConfirmView } from "./confirm";
 
-test("clears the confirm window", () => {
-  const confirmWindow = document.createElement("div");
-  confirmWindow.id = "notification-div";
-  document.body.appendChild(confirmWindow);
-  confirmWindow.innerText = "CONFIRM TEXT";
-  hideConfirmView();
-  expect(confirmWindow.outerHTML).toBe('<div id="notification-div"></div>');
-  expect(confirmWindow.id).toBe("notification-div");
-  expect(confirmWindow.innerText).toBe("");
+describe('close confirmation function', () => {
+  test("clears the confirm window", () => {
+    const confirmWindow = document.createElement("div");
+    confirmWindow.id = "notification-div";
+    document.body.appendChild(confirmWindow);
+    confirmWindow.innerText = "CONFIRM TEXT";
+    hideConfirmView();
+    expect(confirmWindow.outerHTML).toBe('<div id="notification-div"></div>');
+    expect(confirmWindow.id).toBe("notification-div");
+    expect(confirmWindow.innerText).toBe("");
+  });
+})
+
+describe('confirmation modal', () => {
+  test("creates the confirm modal", () => {
+    const action = jest.fn();
+    const msg = "delete?";
+    expect(confirm(msg, action).id).toBe("confirm");
+  });
 });
 
-test("creates the confirm modal", () => {
-  const action = jest.fn();
-  const msg = "delete?";
-  expect(confirm(msg, action).id).toBe("confirm");
-});
---
+++ src/todo.test.js
@@ -0,0 +1,8 @@
+import Todo from "./todo";
+
+describe("test Todo class", () => {
+  test("assigns the instance variables", () => {
+    const todo = new Todo("title", "description", "dueDate", "priority");
+    expect(todo.title).toBe("title");
+  });
+});
